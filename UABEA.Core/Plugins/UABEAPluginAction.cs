bde14dafaffaeabe83afdeda030a2b0341badd0c
SukmaUmbaran17 <110231525+SukmaUmbaran17@users.noreply.github.com>
2026-08-04 17:58:36 +0700

    Create UABEAPluginAction.cs

---
+++ UABEA.Core/Plugins/UABEAPluginAction.cs
@@ -0,0 +1,11 @@
+namespace UABEA.Core.Plugins
+{
+    public enum UABEAPluginAction
+    {
+        Import = 1,
+        Export = 2,
+        Console = 4,
+        Create = 8,
+        All = 15
+    }
+}
