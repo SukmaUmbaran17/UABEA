e47c7efb9c393b4b6984d4fe456915edb488738f
SukmaUmbaran17 <110231525+SukmaUmbaran17@users.noreply.github.com>
2026-08-20 06:37:42 +0700

    Remove external storage permissions from MainActivity
    Removed unnecessary permissions for external storage.

--- AVALONIAAndroid/MainActivity.cs
+++ AVALONIAAndroid/MainActivity.cs
@@ -1,14 +1,8 @@
 using Android.App;
 using Android.Content.PM;
-using Android;
 using Avalonia;
 using Avalonia.Android;
 using UABEAvalonia;
-
-// Menyuntikkan izin penyimpanan langsung via kode C# tanpa membutuhkan AndroidManifest.xml
-[assembly: UsesPermission(Manifest.Permission.ReadExternalStorage)]
-[assembly: UsesPermission(Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Manifest.Permission.ManageExternalStorage)]
 
 namespace AVALONIAAndroid;
 
