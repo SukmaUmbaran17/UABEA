6218353f2188a4a32e232c91b2b8cb6fa72a6ccd
SukmaUmbaran17 <110231525+SukmaUmbaran17@users.noreply.github.com>
2026-08-20 07:14:45 +0700

    Add storage permissions and application attributes
    Added permissions for external storage access and set application attributes.

--- AVALONIAAndroid/MainActivity.cs
+++ AVALONIAAndroid/MainActivity.cs
@@ -1,8 +1,20 @@
 using Android.App;
 using Android.Content.PM;
+using Android;
 using Avalonia;
 using Avalonia.Android;
 using UABEAvalonia;
+
+// 1. Menyuntikkan Izin Penyimpanan Memori HP
+[assembly: UsesPermission(Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Manifest.Permission.ManageExternalStorage)]
+
+// 2. Menyuntikkan Atribut Aplikasi Utama agar .NET Membuatkan Manifes Otomatis yang Bersih
+[assembly: Application(
+    Label = "UABEA Android Native",
+    AllowBackup = true,
+    RequestLegacyExternalStorage = true)]
 
 namespace AVALONIAAndroid;
 
