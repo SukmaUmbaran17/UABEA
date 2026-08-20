7792f569a76e16b0b0fb3df2f7d2852cfb246625
SukmaUmbaran17 <110231525+SukmaUmbaran17@users.noreply.github.com>
2026-08-20 12:17:21 +0700

    Reorganize using directives in MainActivity.cs

--- AVALONIAAndroid/MainActivity.cs
+++ AVALONIAAndroid/MainActivity.cs
@@ -2,13 +2,12 @@ using Android.App;
 using Android.Content.PM;
 using Avalonia;
 using Avalonia.Android;
-using UABEAvalonia; // Menghubungkan ke logika utama UABEA
+using UABEAvalonia;
 
 namespace AVALONIAAndroid;
 
 [Activity(
     Label = "UABEA Android Native",
-    Theme = "@style/AvaloniaTheme",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity<App>
